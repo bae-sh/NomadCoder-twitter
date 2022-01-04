028ee8a8940058867bd9cdacde4af6292a14feae
bae-sh <hyeon1293@gmail.com>
2022-01-05 01:29:34 +0900

    add firestore

---
+++ src/routes/Profile.js
@@ -0,0 +1,16 @@
+import { useHistory } from "react-router-dom";
+import { authService } from "../fbase";
+
+const Profile = () => {
+    const history = useHistory();
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    };
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+};
+export default Profile;
