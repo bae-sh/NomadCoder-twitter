97077c02ad4bc4c219b918998d11628b96285c40
bae-sh <hyeon1293@gmail.com>
2022-01-05 13:17:54 +0900

    add get doc

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../fbase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, query, getDocs, doc } from "firebase/firestore";
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const q = query(collection(dbService, "nweets"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            const nweetObj = {
+                ...doc.data(),
+                id: doc.id,
+            };
+            console.log(doc);
+            setNweets((prev) => [nweetObj, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getNweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await addDoc(collection(dbService, "nweets"), {
@@ -29,6 +45,13 @@ const Home = () => {
                 />
                 <input type={"submit"} value={"Nweet"} />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
